f53f35a6e1749c0a275c0d23852761822e3cbb04
Daniel Milnes <thebeanogamer@gmail.com>
2020-02-27 23:19:00 +0000

    Render index

--- quoteswebsite/__init__.py
+++ quoteswebsite/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 def main():
     app.run(host="0.0.0.0")
