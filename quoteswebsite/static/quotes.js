99fc19e04606ecb71035fd8c62c27dba99aa73cb
Daniel Milnes <thebeanogamer@gmail.com>
2020-02-29 17:12:44 +0000

    Finish basic frontend

--- quoteswebsite/static/quotes.js
+++ quoteswebsite/static/quotes.js
@@ -1,5 +1,12 @@
 (function quotes() {
-            quote = $.getJSON("/api/quote");
-            $("#quote").text(quote.text);
-            $("#author").text(quote.author);
-})(); 
+    quote = $.getJSON("/api/quote");
+    $("#quote").text(quote.text);
+    $("#author").text("- " + quote.author);
+    $("#permalink_input").val(window.location.hostname + "/quote/" + quote.id);
+
+    $("#permalink_button").click(function () {
+        input = $("#permalink_input")
+        input.select();
+        document.execCommand("copy");
+    });
+})();
